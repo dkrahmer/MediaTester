0bf40fea0cf39e546f56645ec99a0194b56ab955
Doug Krahmer <doug.github@remhark.com>
2022-04-23 10:09:39 -0700

    Add auto build scripts (#8)

--- build.sh
+++ build.sh
@@ -59,72 +59,49 @@ Initialize()
 
 	SlnFile="MediaTester.sln"
 
-	if [[ "$IS_BUILD_VS2019" == "true" ]]; then
+	if [[ "$NUGET" == "" ]]; then
+		# The nuget/setup-nuget build step sets $NUGET
+		NUGET=nuget.exe
+	fi
+
+	if [[ "$IS_BUILD" == "true" ]] && [[ "$MSBuildPath" == "" ]]; then
 		# List of MSBuild paths. Preferred paths listed first.
-		MSBuildPath_VS2019=$(GetFirstFile \
+		MSBuildPath=$(GetFirstFile \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" \
-					)
-
-		if [[ "${MSBuildPath_VS2019}" == "" ]]; then
-			echo "Could not find MSBuild.exe in any expected paths."
-			echo "Ensure MS Build Tools or Visual Studio 2019 Professional is installed. (MSBuild 16+ required)"
-			exit 1
-		fi
-
-		echo "Using MSBuild: ${MSBuildPath_VS2019}"
-	fi
-
-	if [[ "$IS_BUILD_VS2017" == "true" ]]; then
-		# List of MSBuild paths. Preferred paths listed first.
-		MSBuildPath_VS2017=$(GetFirstFile \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" \
 					)
 
-		if [[ "${MSBuildPath_VS2017}" == "" ]]; then
+		if [[ "${MSBuildPath}" == "" ]]; then
 			echo "Could not find MSBuild.exe in any expected paths."
-			echo "Ensure MS Build Tools or Visual Studio 2017 Professional is installed."
+			echo "Ensure MS Build Tools or Visual Studio Professional is installed. (MSBuild 15-16.* required / VS2017-VS2019)"
 			exit 1
 		fi
 
-		echo "Using MSBuild (VS2017): ${MSBuildPath_VS2017}"
+		echo "Using MSBuild: ${MSBuildPath}"
 	fi
 
-	if [[ "$ENABLE_UNIT_TESTS_VS2019" == "true" ]]; then
+	if [[ "$ENABLE_UNIT_TESTS" == "true" ]]; then
 		# List of \vstest.console.exe paths. Preferred paths listed first.
-		VsTestPath_VS2019=$(GetFirstFile \
+		VsTestPath=$(GetFirstFile \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\Extensions\TestPlatform\vstest.console.exe" \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\Extensions\TestPlatform\vstest.console.exe" \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\Extensions\TestPlatform\vstest.console.exe" \
-					)
-
-		if [[ "${VsTestPath_VS2019}" == "" ]]; then
-			echo "Could not find vstest.console.exe in any expected paths."
-			echo "Ensure MS Build Tools 2019 or Visual Studio 2019 Professional is installed."
-			exit 1
-		fi
-
-		echo "Using VsTest 2019: ${VsTestPath_VS2019}"
-	fi
-
-	if [[ "$ENABLE_UNIT_TESTS_VS2017" == "true" ]]; then
-		# List of \vstest.console.exe paths. Preferred paths listed first.
-		VsTestPath_VS2017=$(GetFirstFile \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\Extensions\TestPlatform\vstest.console.exe" \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\Extensions\TestPlatform\vstest.console.exe" \
 					"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\Extensions\TestPlatform\vstest.console.exe" \
 					)
 
-		if [[ "${VsTestPath_VS2017}" == "" ]]; then
+		if [[ "${VsTestPath}" == "" ]]; then
 			echo "Could not find vstest.console.exe in any expected paths."
-			echo "Ensure MS Build Tools 2017 or Visual Studio 2017 Professional is installed."
+			echo "Ensure MS Build Tools or Visual Studio is installed."
 			exit 1
 		fi
 
-		echo "Using VsTest 2017: ${VsTestPath_VS2017}"
+		echo "Using VsTest: ${VsTestPath}"
 	fi
 
 	ProgressEnd "$progressName"
@@ -168,7 +145,7 @@ POSITIONAL=()
 dateStr=$(date '+%Y-%m-%d_%H%M%S')
 BUILD_LOG_FILE="build-output_${dateStr}.log"
 SHOW_EXTRA_INFO=true
-#ENABLE_UNIT_TESTS_VS2017=true
+#ENABLE_UNIT_TESTS=true
 ENABLE_ANSI=true
 ENABLE_CLEAN=true
 ENABLE_LIST_OUTPUT=true
@@ -179,27 +156,32 @@ do
 	key="$1"
 
 	case $key in
+		--msbuild-path)
+			shift
+			MSBuildPath="$1"
+			shift
+			;;
 		--restore)
 			RUN_INITIALIZE=true
-			IS_BUILD_VS2017=true
+			IS_BUILD=true
 			RESTORE=true
 			shift
 			;;
 		--build)
 			RUN_INITIALIZE=true
 			LIST_OUTPUT=true
-			IS_BUILD_VS2017=true
+			IS_BUILD=true
 			BUILD=true
 			shift
 			;;
 		--clean)
 			RUN_INITIALIZE=true
-			IS_BUILD_VS2017=true
+			IS_BUILD=true
 			CLEAN=true
 			shift
 			;;
 		#--no-test)
-		#	ENABLE_UNIT_TESTS_VS2017=false
+		#	ENABLE_UNIT_TESTS=false
 		#	shift
 		#	;;
 		#--test)
@@ -221,7 +203,7 @@ do
 		--all)
 			RUN_INITIALIZE=true
 			LIST_OUTPUT=true
-			IS_BUILD_VS2017=true
+			IS_BUILD=true
 			CLEAN=true
 			RESTORE=true
 			BUILD=true
@@ -300,16 +282,16 @@ fi
 	fi
 
 	if [[ "$ENABLE_CLEAN" == "true" ]] && [[ "$CLEAN" == "true" ]]; then
-		RunSolutionTarget "${SlnFile}" "Clean" "${MSBuildPath_VS2017}"
+		RunSolutionTarget "${SlnFile}" "Clean" "${MSBuildPath}"
 	fi
 
 	if [[ "$RESTORE" == "true" ]]; then
-		#RunSolutionTarget "${SlnFile}" "Restore" "${MSBuildPath_VS2017}"
-		BuildTools/nuget restore "${SlnFile}"
+		#RunSolutionTarget "${SlnFile}" "Restore" "${MSBuildPath}"
+		"$NUGET" restore "${SlnFile}"
 	fi
 
 	if [[ "$BUILD" == "true" ]]; then
-		RunSolutionTarget "${SlnFile}" "Build" "${MSBuildPath_VS2017}"
+		RunSolutionTarget "${SlnFile}" "Build" "${MSBuildPath}"
 		
 		mkdir -p _output/MediaTesterGui
 		mkdir -p _output/MediaTesterCli
@@ -320,14 +302,14 @@ fi
 		cp MediaTesterLib/bin/Release/*.dll _output/MediaTesterLib/
 	fi
 
-	if [[ "$ENABLE_UNIT_TESTS_VS2017" == "true" ]]; then
-		if [[ "$RUN_UNIT_TESTS_FRAMEWORK" == "true" ]]; then
-			TestFramework
-		fi
-		if [[ "$RUN_UNIT_TESTS_CORE" == "true" ]]; then
-			TestCore
-		fi
-	fi
+	#if [[ "$ENABLE_UNIT_TESTS" == "true" ]]; then
+	#	if [[ "$RUN_UNIT_TESTS_FRAMEWORK" == "true" ]]; then
+	#		TestFramework
+	#	fi
+	#	if [[ "$RUN_UNIT_TESTS_CORE" == "true" ]]; then
+	#		TestCore
+	#	fi
+	#fi
 
 	if [[ "$IS_UNRELEASE" == "true" ]]; then
 		UnPrepareProjectsForRelease
@@ -343,7 +325,7 @@ fi
 		if [ -d "_output" ]; then
 			echo ""
 			EchoInColor "_output:"
-			ls -goh "_output"
+			ls -hoRg "_output"
 		fi
 	fi
 } 2>&1 | tee "${BUILD_LOG_FILE}"
